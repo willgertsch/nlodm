56fa96b68ecfd3cc1bb8fcadb9e81adf7ca8989d
willgertsch <willgertsch+github@gmail.com>
2023-10-03 12:23:21 -0700

    added loglogistic test case

--- tests/testthat/test-nlodm.R
+++ tests/testthat/test-nlodm.R
@@ -12,9 +12,29 @@ test_that('logistic_D', {
     iter = 100,
     seed = 1234
   )
-  out$result
-  out$plot
+  #out$result
+  #out$plot
 
   expect_equal(length(out$result$result), 4)
+  expect_equal(length(out$plot), 9) # ggplot objects are lists of length 9
+})
+
+test_that('loglogistic_D', {
+  out = nlodm(
+    model = NULL,
+    grad_fun = grad.loglogistic,
+    obj = 'D',
+    theta = c(0.02461, -2.390, 1),
+    bound = 30,
+    pts = 3,
+    algorithm = 'PSO',
+    swarm = 30,
+    iter = 400,
+    seed = 1234
+  )
+  #out$result
+  #out$plot
+
+  expect_equal(length(out$result$result), 6)
   expect_equal(length(out$plot), 9) # ggplot objects are lists of length 9
 })
