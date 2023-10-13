7dd6d56b068aad8a24ec62b7b0d4e3375e5afc13
willgertsch <willgertsch+github@gmail.com>
2023-10-13 12:03:23 -0700

    finished multiobjective function factory and wrote test

--- R/multi_obj_fun_factory.R
+++ R/multi_obj_fun_factory.R
@@ -1,18 +1,28 @@
 # function for constructing objective function for use with algorithms
+# multi-objective: construct function that returns a vector
 # M_fun: information matrix function
 # obj_fun: objective function of information matrix
 # theta: parameter values to pass through to M_fun
 # par: other parameters, such as c values for c objective
-obj_fun_factory = function(grad_funs, obj_funs, thetas, params) {
+multi_obj_fun_factory = function(grad_funs, obj_funs, thetas, params) {
 
   # these are used in interface function
   force(grad_funs)
   force(thetas)
   force(params)
 
-  # interface called by optimization software
+  # check dimensions of the problem are correct
+  d = length(obj_funs)
+  if (length(grad_funs) !=  d)
+    stop("number of objective functions doesn't match number of gradient functions")
+  else if (length(thetas) != d)
+    stop("number of theta arrays doesn't match number of objectives")
+  else if (length(params) != d)
+    stop("number of additional parameter vectors doesn't match number of objectives")
+
+  # interface called by optimization software: rmoo
   # return this function
-  function(vars, ...) {
+  function(vars, nobj = d, ...) {
     # distinguish between points and weights
     pts = length(vars)/2
     x = vars[1:pts]
@@ -21,24 +31,22 @@ obj_fun_factory = function(grad_funs, obj_funs, thetas, params) {
     # check weight constraint
     s = sum(w, na.rm = T) # na.rm needed to fix if statement error
     if (s > 1) # constraint implementation
-      return(-Inf)
+      return(rep(-Inf, nobj))
 
     M_fun = M.nonlinear # always using general nonlinear matrix
 
     # average over prior theta values
     # call objective functions
-    obj_vals = list()
+    obj_vals = numeric(nobj)
     for (i in 1:length(grad_funs)) {
-      obj_vals[i] = obj_fun(M_fun(x, w, thetas[i], grad_funs[i]), params[i])
+      obj_vals[i] = obj_funs[[i]](M_fun(x, w, thetas[[i]], grad_funs[[i]]), params[[i]])
     }
-
 
     # deal with missing
     # vectorize
-    if (is.na(obj_value))
-      return(-Inf)
-    else
-      return(obj_value)
+    nas = is.na(obj_vals)
+    obj_vals[nas] = -Inf
+    return(obj_vals)
   }
 
 }
---
+++ tests/testthat/test-multi_obj_fun_factory.R
@@ -0,0 +1,35 @@
+test_that("multiobjective function", {
+
+  grad_funs = list(grad.logistic, grad.loglogistic, grad.weibull)
+  obj_funs = list(obj.A, obj.D, obj.D)
+  thetas = list(
+    matrix(c(-1.710, 0.09703), nrow=1),
+    matrix(c(0.02461, -2.390, 1), nrow=1),
+    matrix(c(0.05307, .99, 0.04929), nrow=1)
+  )
+  params = list(
+    c(),
+    c(),
+    c()
+  )
+  test_multi_fun = multi_obj_fun_factory(grad_funs, obj_funs, thetas, params)
+
+  # uniform 3 point design
+  x = c(0.1, 15, 30)
+  w = c(1/3, 1/3, 1/3)
+  vars = c(x, w)
+
+  test_obj_vals = test_multi_fun(vars)
+
+  true_obj_vals = numeric(3)
+  obj_fun1 = obj_fun_factory(grad_funs[[1]], obj_funs[[1]], thetas[[1]], params[[1]])
+  obj_fun2 = obj_fun_factory(grad_funs[[2]], obj_funs[[2]], thetas[[2]], params[[2]])
+  obj_fun3 = obj_fun_factory(grad_funs[[3]], obj_funs[[3]], thetas[[3]], params[[3]])
+  true_obj_vals[1] = obj_fun1(vars)
+  true_obj_vals[2] = obj_fun2(vars)
+  true_obj_vals[3] = obj_fun3(vars)
+
+  # compare
+  expect_equal(true_obj_vals, test_obj_vals)
+
+})
