06194b6c3f4a6376b909468f9a13b33b2db7cc7a
willgertsch <willgertsch+github@gmail.com>
2023-10-12 19:58:05 -0700

    created a larger Bayesian test case

--- tests/testthat/test-bayesian.R
+++ tests/testthat/test-bayesian.R
@@ -74,3 +74,42 @@ test_that('Bayesian D logistic from prior', {
 
 
 })
+
+test_that('Bayesian D logistic from prior N=100', {
+
+  set.seed(1234)
+  N = 100
+  theta0 = rnorm(N, -1.710, .3)
+  theta1 = rnorm(N, 0.09703, .01)
+
+  # plot
+  # step = 30/1000
+  # xvals = seq(0, 30, step)
+  # plot(1/(1 + exp(-theta0[1] - theta1[1]*xvals)) ~ xvals)
+  # for (i in 2:N)
+  #   lines(1/(1 + exp(-theta0[i] - theta1[i]*xvals)) ~ xvals)
+
+  thetam = cbind(theta0, theta1)
+  prior_weights = rep(1/N, N)
+
+  out = nlodm(
+    model = NULL,
+    grad_fun = grad.logistic,
+    obj = 'D',
+    theta = thetam,
+    prior_weights = prior_weights,
+    bound = 30,
+    pts = 4,
+    algorithm = 'PSO',
+    swarm = 20,
+    iter = 100,
+    seed = 1234
+  )
+  #out$plot
+
+  expect_equal(length(out$result$result), 4)
+  expect_equal(length(out$plot), 9) # ggplot objects are lists of length 9
+
+
+
+})
