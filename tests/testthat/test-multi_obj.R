3f913934cbc2f1e4641a7601864d4a0702456ec5
willgertsch <willgertsch+github@gmail.com>
2023-10-27 09:32:55 -0700

    BMD test case

--- tests/testthat/test-multi_obj.R
+++ tests/testthat/test-multi_obj.R
@@ -35,3 +35,38 @@ test_that("basic multi_obj", {
   expect_equal(length(p), 9) # ggplot objects are lists of length 9
 
 })
+
+test_that("multi obj BMD", {
+
+  grad_funs = list(grad.loglogistic, grad.loglogistic)
+  obj_funs = list(obj.D, obj.c)
+  bmd_grad = get_bmd_grad("Log-logistic", 'added')
+  theta = c(0.02461, -2.390, 1)
+  thetas = list(
+    theta,
+    theta
+  )
+  params = list(
+    c(),
+    bmd_grad(0.1, theta)
+  )
+
+  result = multi_obj(
+    grad_funs = grad_funs,
+    obj_funs = obj_funs,
+    thetas = thetas,
+    params = params,
+    type = 'pareto',
+    bound = 30,
+    pts = 3,
+    swarm = 50,
+    maxiter = 100,
+    verbose = F
+  )
+
+  pareto_data = extract_nsga2R(result)
+  p = plot_pareto2d(pareto_data, c("D", "c"))
+
+  expect_equal(ncol(pareto_data), 8)
+  expect_equal(length(p), 9)
+})
